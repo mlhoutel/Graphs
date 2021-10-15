cf2248e1aaeddee6a2fdd6e639f00b62a213b35b
LHOUTELLIER Maël <55429341+mlhoutel@users.noreply.github.com>
2021-10-15 20:12:20 +0200

    feat: :art: Style et Layout automatique de l'affichage

--- src/main/java/Drawing/Canvas.java
+++ src/main/java/Drawing/Canvas.java
@@ -1,27 +1,46 @@
 package Drawing;
 
-import javax.swing.*;
+import java.util.Map;
 
-import org.jgraph.*;
+import java.awt.BorderLayout;
+import javax.swing.*;
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.util.mxConstants;
+import com.mxgraph.layout.hierarchical.mxHierarchicalLayout;
+import com.mxgraph.view.mxGraph;
 
 public class Canvas extends JFrame {
 
     /** Pour éviter un warning venant du JFrame */
     private static final long serialVersionUID = -8123406571694511514L;
-    public JGraph graph;
-
-    public Canvas() {
-        super("Default window name");
-        this.graph = new JGraph();
-    }
+    public mxGraph graph;
+    public mxGraphComponent component;
 
     public Canvas(String name) {
         super(name);
-        this.graph = new JGraph();
+        this.graph = new mxGraph();
+        this.component = new mxGraphComponent(this.graph);
     }
 
     public void Draw() {
-        this.getContentPane().add(new JScrollPane(this.graph));
+        this.add(new JScrollPane(this.component), BorderLayout.CENTER);
+        // getContentPane().add(this.component);
+        
+        new mxHierarchicalLayout(this.graph).execute(this.graph.getDefaultParent());
+        
+        Map<String, Object> edges = this.graph.getStylesheet().getDefaultEdgeStyle();
+        edges.put(mxConstants.STYLE_STROKECOLOR , "#ad8aa8");
+        edges.put(mxConstants.STYLE_STROKEWIDTH, 3);
+
+        Map<String, Object> vertexs = this.graph.getStylesheet().getDefaultVertexStyle();
+        vertexs.put(mxConstants.STYLE_FILLCOLOR, "#1f8bdd");
+        vertexs.put(mxConstants.STYLE_STROKECOLOR, "#1f8bdd");
+        vertexs.put(mxConstants.STYLE_FONTCOLOR, "#edf1f4");
+        vertexs.put(mxConstants.STYLE_FONTSIZE, 17);
+        vertexs.put(mxConstants.STYLE_FONTFAMILY, "Roboto");
+        vertexs.put(mxConstants.STYLE_FONTSTYLE, mxConstants.FONT_BOLD);
+        vertexs.put(mxConstants.STYLE_ROUNDED, true);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(800, 600);
         this.setVisible(true);
--- src/main/java/Drawing/GraphAlgorithms/DrawBinaryHeap.java
+++ src/main/java/Drawing/GraphAlgorithms/DrawBinaryHeap.java
@@ -5,56 +5,41 @@ import Graphs.GraphAlgorithms.BinaryHeap;
 
 public class DrawBinaryHeap {
     
-    public static void VisitDisplay(BinaryHeap bin, Canvas canvas, Object root, Object parent, int index, int depth, int width) {
-        /*
-        int height = 20;
-        int width = 20;
-        
+    public static void VisitDisplay(BinaryHeap bin, Canvas canvas, Object root, Object parent, int index) {
+
+        Object current = canvas.graph.insertVertex(root, null, bin.nodes.get(index).toString(), 0, 0, 60, 30);
+        canvas.graph.insertEdge(root, null, "", parent, current);
+
         if (bin.hasFirstChild(index)) {
             int childIndex = bin.getFirstChildIndex(index);
-        
-            int baseX = depth * 100
-            int baseY = width * 100
-            
-            Object child = canvas.graph.insertVertex(root, null, bin.nodes.get(childIndex).toString(), baseX, baseY, baseX + width, baseY + width);
-            Object edge = canvas.graph.insertEdge(root, null, "", parent, child);
-        
-            VisitDisplay(bin, canvas, child, childIndex, depth+1, width);
+            VisitDisplay(bin, canvas, root, current, childIndex);
         }
         
         if (bin.hasSecondChild(index)) {
             int childIndex = bin.getSecondChildIndex(index);
-            
-            int baseX = depth * 100
-            int baseY = width * 100
-            
-            Object child = canvas.graph.insertVertex(root, null, bin.nodes.get(childIndex).toString(), baseX, baseY, baseX + width, baseY + width);
-            Object edge = canvas.graph.insertEdge(root, null, "", parent, child);
-        
-            VisitDisplay(bin, canvas, child, childIndex, depth+1, width + 1);
-        }*/
+            VisitDisplay(bin, canvas, root, current, childIndex);
+        }
     }
 
     public static void Display(BinaryHeap bin) {
-        /*
         Canvas canvas = new Canvas("Binary Heap");
         canvas.graph.getModel().beginUpdate();
         
         try {
-            Object parent = graph.getDefaultParent();
-            VisitDisplay(bin, canvas, parent, parent, 0, 0, 0);
+            Object parent = canvas.graph.getDefaultParent();
+            VisitDisplay(bin, canvas, parent, parent, 0);
         }
         finally {
-            graph.getModel().endUpdate();
-            graph.Draw();
+            canvas.graph.getModel().endUpdate();
+            canvas.graph.setCellsEditable(false);
+            canvas.graph.setCellsMovable(false);
+            canvas.Draw();
         }
-        */
     }
     public static void main(String[] args) {
         BinaryHeap jarjarBin = new BinaryHeap();
         System.out.println(jarjarBin.isEmpty() + "\n");
-        int k = 20;
-        int m = k;
+        int k = 100;
         int min = 2;
         int max = 20;
         while (k > 0) {
@@ -65,14 +50,8 @@ public class DrawBinaryHeap {
             jarjarBin.insert(rand);
             k--;
         }
-        // A completer
-        System.out.println("\n" + jarjarBin);
-        System.out.println("removing...");
-        System.out.println("removed value"+jarjarBin.remove());
-        System.out.println("\n" + jarjarBin);
-        System.out.println(jarjarBin.test());
 
-        //Display(jarjarBin);
+        Display(jarjarBin);
     }
 
 }
