5197eedb1550cea2a6f735a10acdd27000bbb74c
LHOUTELLIER Maël <55429341+mlhoutel@users.noreply.github.com>
2021-12-23 13:50:53 +0100

    Fix tests

--- src/test/java/Algorithms/DijkstraTest.java
+++ src/test/java/Algorithms/DijkstraTest.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import Drawing.AdjacencyList.DrawGraph;
+import Graphs.AdjacencyList.DirectedGraph;
 import Graphs.AdjacencyList.DirectedValuedGraph;
 import Graphs.Algorithms.*;
 import Graphs.Nodes.DirectedNode;
@@ -23,23 +24,6 @@ public class DijkstraTest {
         DirectedValuedGraph al = new DirectedValuedGraph(mat);
 
         int From = 7;
-        int To = 0;
-
-        //WHENZEN
-        Exception exception = Assertions.assertThrows(DijkstraException.class, () -> {
-            List<DirectedNode> path = Dijkstra.ShortestPath(al, al.getNodes().get(From), al.getNodes().get(To));
-        });
-
-        //THEN
-        Assertions.assertEquals(DijkstraException.NO_PATH_MSG, exception.getMessage());
-    }
-
-    @Test
-    void Dijkstra_should_throw_no_path_2() throws DijkstraException {
-        int[][] mat = GraphTools.generateValuedGraphData(10, true, false, true, false, 100001);
-        DirectedValuedGraph al = new DirectedValuedGraph(mat);
-
-        int From = 7;
         int To = 1;
 
         //WHENZEN
@@ -53,18 +37,22 @@ public class DijkstraTest {
 
     @Test
     void Dijkstra_ok() throws DijkstraException {
-        int[][] mat = GraphTools.generateValuedGraphData(10, true, false, true, false, 100001);
-        DirectedValuedGraph al = new DirectedValuedGraph(mat);
-
+        //GIVEN
+        int[][] mat = GraphTools.generateValuedGraphData(5, false, false, true, false, 100001);
+        DirectedGraph al = new DirectedGraph(mat);
         int From = 1;
-        int To = 7;
+        int To = 4;
 
-        //WHENZEN
-        Exception exception = Assertions.assertThrows(DijkstraException.class, () -> {
-            List<DirectedNode> path = Dijkstra.ShortestPath(al, al.getNodes().get(From), al.getNodes().get(To));
-        });
+        //WHEN
+        List<DirectedNode> path = Dijkstra.ShortestPath(al, al.getNodes().get(From), al.getNodes().get(To));
 
         //THEN
-        Assertions.assertEquals(DijkstraException.NO_PATH_MSG, exception.getMessage());
+        List<DirectedNode> expected = new ArrayList<>();
+        expected.add(al.getNodes().get(1));
+        expected.add(al.getNodes().get(2));
+        expected.add(al.getNodes().get(0));
+        expected.add(al.getNodes().get(4));
+
+        Assertions.assertEquals(expected, path);
     }
 }
